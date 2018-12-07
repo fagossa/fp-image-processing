f05b011fb842c8eaec41bf6da6fc9ff1049e2346
fagossa <fagossa@gmail.com>
2018-12-07 13:32:25 +0100

    Add first impl for 'PImage'

--- src/main/scala/com/fabian/icarus/img2/fp.scala
+++ src/main/scala/com/fabian/icarus/img2/fp.scala
@@ -13,6 +13,21 @@ object fp {
       Image(w, h, data.updated(x * h + y, value))
   }
 
+  case class PImage[T](x: Int, y: Int, image: Image[T]) {
+    def extract: T = image(x, y)
+
+    def map[S](f: T => S): PImage[S] = PImage(x, y, image map f)
+
+    def coflatMap[S](f: PImage[T] => S): PImage[S] = {
+      val data = (0 until (image.w * image.h)).toVector.par.map { i =>
+        val xx = i / image.h
+        val yy = i % image.h
+        f(PImage(xx, yy, image))
+      }
+      PImage(x, y, Image(image.w, image.h, data))
+    }
+  }
+
 }
 
 object filters {
